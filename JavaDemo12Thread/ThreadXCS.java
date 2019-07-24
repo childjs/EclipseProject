b226568b177ec4b93302552f90e7d54b6554550f
childjs <171050187@qq.com>
2019-07-24 22:31:00 +0800

    线程修改测试提交

--- JavaDemo12Thread/ThreadXCS.java
+++ JavaDemo12Thread/ThreadXCS.java
@@ -51,11 +51,11 @@ public class ThreadXCS implements Runnable {
 		ThreadXCS tx = new ThreadXCS();
 		Thread th = new Thread(tx, "售票窗口一");
 		Thread th2 = new Thread(tx,"售票窗口二");
-		Thread th3 = new Thread(tx, "售票窗口三");
-		Thread th4 = new Thread(tx, "售票窗口四");
+		//Thread th3 = new Thread(tx, "售票窗口三");
+		//Thread th4 = new Thread(tx, "售票窗口四");
 		th.start();
 		th2.start();
-		th3.start();
-		th4.start();
+		//th3.start();
+		//th4.start();
 	}
 }
